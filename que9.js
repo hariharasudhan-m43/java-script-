20526265cd4a0b507703d2a39f02cd5c4d6e12f8
hariharasudhan-m43 <sq.hariharasudhan@gmail.com>
2025-07-17 03:24:09 -0400

    Update que9.js

---
+++ que9.js
@@ -0,0 +1,10 @@
+function removeDuplicates(arr) {
+    let uniqueArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueArr.includes(arr[i])) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    return uniqueArr;
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5, 6, 6]));
